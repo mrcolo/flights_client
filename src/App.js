638fe0af6674a5e6ceaf6c98f513b264cacd7e43
mrcolo <colo28197@gmail.com>
2018-12-05 16:40:38 -0800

    added ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-import { Button } from 'semantic-ui-react'
+import { Button, Grid, Input, Container, Label, Segment, Header } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Map
-        centerAroundCurrentLocation
-        className="map"
-        google={this.props.google}
-        style={{ height: '100%', position: 'relative', width: '100%' }}
-        zoom={14}
-      >
-      <Marker
-  title={'The marker`s title will appear as a tooltip.'}
-  name={'SOMA'}
-  position={{lat: 37.778519, lng: -122.405640}} />
-      </Map>
-        </header>
+          <Segment vertical>
+                <Header> ✈️ FLIGHT PLANNER ✈️</Header>
+                <Input label='From' style={{padding: 10}} size='big'/>
+                <Input label='To' style={{padding: 10}} size='big'/>
+                <Button color='yellow' size='big' >
+                  Get Itinerary
+                </Button>
+          </Segment>
+            <Map
+              centerAroundCurrentLocation
+              className="map"
+              google={this.props.google}
+              style={{ height: '100%', position: 'relative', width: '100%' }}
+              zoom={14}
+            >
+            </Map>
       </div>
     );
   }
